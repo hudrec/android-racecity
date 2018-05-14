d49439fb44223868a9e37c6aa9fba4b89622b61e
jqdaga <jqdaga@gmail.com>
2018-05-13 23:36:24 -0500

    activity control

---
+++ app/src/main/java/pe/edu/upc/racecity/Activities/ControlActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.racecity.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.upc.racecity.R;
+
+public class ControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
+}
